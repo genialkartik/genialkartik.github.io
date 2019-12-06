f11ac080c48fc5168ca6c1584192f0044cf5996b
Kartik Tyagi <kartik9756@gmail.com>
2019-12-06 06:03:11 +0530

    Update index.js

--- jquery/index.js
+++ jquery/index.js
@@ -4,6 +4,15 @@ if(e.ctrlKey)
   event.preventDefault();//prevent zoom
 });
 $(document).ready(function(){
+
+
+// Drag and Resize
+  $( function() {
+    $( ".wind-con").draggable();
+    $( ".wind-con").resizable();
+  });
+
+
 // Chaining
 $(".upper-docker-conn").animate({top: '0'}, "slow");
 $(".docker-container").animate({bottom: '0'}, "fast",)
@@ -27,9 +36,7 @@ function rminitiatedemo(){
 $(".wind-con").hide();
 
 
-
-var temp;
-
+// Lower DOCKER ANimation
 $(".docker-conn ul li").click(function(){
 $(this).animate({top: '-20px'}, "fast");
 $(this).animate({top: '0px'} , "fast");
@@ -37,116 +44,58 @@ $(this).animate({top: '-3px'}, "fast" );
 $(this).animate({top: '0px'} , "fast");
 
 });
-$(".abo-doc").click(function(){
-  temp = $("#abo-window");
-  wmanager(temp);
-});
-$(".aca-doc").click(function(){
-  temp = $("#aca-window");
-  wmanager(temp);
-});
-$(".ski-doc").click(function(){
-  temp = $("#ski-window");
-  wmanager(temp);
-});
-$(".lan-doc").click(function(){
-  temp = $("#lan-window");
-  wmanager(temp);
-});
-$(".sof-doc").click(function(){
-  temp = $("#sof-window");
-  wmanager(temp);
-});
-$(".exp-doc").click(function(){
-  temp = $("#exp-window");
-  wmanager(temp);
-});
-$(".pro-doc").click(function(){
-  temp = $("#pro-window");
-  wmanager(temp);
+// On lower docket click
+$(".dcimg").click(function(){
+  var dcimgcls = $(this).prop('class').toString();
+  var dcimgvar = dcimgcls.substring(dcimgcls.length-2, dcimgcls.length);
+  wmanager(dcimgvar);
 });
 
-$(".ach-doc").click(function(){
-  temp = $("#ach-window");
-  wmanager(temp);
-});
-$(".cou-doc").click(function(){
-  temp = $("#cou-window");
-  wmanager(temp);
-});
-$(".hob-doc").click(function(){
-  temp = $("#hob-window");
-  wmanager(temp);
-});
-$(".con-doc").click(function(){
-  temp = $("#con-window");
-  wmanager(temp);
-});
-$(".gal-doc").click(function(){
-  temp = $("#gal-window");
-  wmanager(temp);
-});
-
-$(".res-doc").click(function(){
-  temp = $("#res-window");
-  wmanager(temp);
-});
-
-$(".help-doc").click(function(){
-  temp = $("#help-window");
-  wmanager(temp);
-});
-
-$(".cont-doc").click(function(){
-  temp = $("#cont-window");
-  wmanager(temp);
-});
-
-$(".blog-doc").click(function(){
-  temp = $("#blog-window");
-  wmanager(temp);
-});
-
-$(".thi-doc").click(function(){
-  temp = $("#thi-window");
-  wmanager(temp);
-});
-
-
-
-function wmanager(t){
-
+var opnwinid = $("#opend-window");
+function wmanager(dcimgvar){
+  var t = $(".opnwin"+dcimgvar);
   //window show hide
-  t.toggle();
-  $(".wind-con").animate({ width:'60%', height: '70%', top: '15%', left: '20%' }, "fast");
+  t.toggle().animate({ width:'60%', height: '70%', top: '15%', left: '20%' }, "fast");
 
   // Cls, mini
-  $("#closetab, #minimztab").click(function(){
-    temp.hide();
-    $(".docker-container").css({ "height": "7.5%"});
-    $(".wind-con").animate({ width:'0%', height: '0%', top: '45%', left: '45%' }, "fast");
+  $(".closetab, .minimztab").click(function(){
+    $(".docker-container").css({ "height": "7.5%"})
+    .promise().done(function(){
+      $(".doc-demo").css({"border-top": "1px solid #424242"});
+    });
+
+    var clsminid = $(this).prop('class').toString();
+    var clsminno = clsminid.substring(clsminid.length-2, clsminid.length);
+    var clsminwin = $(".opnwin"+clsminno);
+    console.log(clsminwin);
+    clsminwin.animate({ width:'0%', height: '0%', top: '45%', left: '45%' }, "fast");
   });
 
 
   //tabsize
   $(".tabsize").click(function(){
-    var tem = parseInt(temp.css("width"));
-    if(tem <= 900){
-      $(temp).css({
+    var twdth = parseInt(t.css("width"));
+    var opnwinwdth = parseInt(opnwinid.css("width"));
+    var tem = twdth/opnwinwdth*100;
+    console.log(tem);
+    if(tem <= 99){
+      $(t).css({
         "width": "100%",
         "height": "100%",
         "top": "0%",
         "left": "0%",
       });
+      $(".dc-lower ul li").css({"border-top": "none"});
       $(".docker-container").css({ "height": "0%"});
       }
-      else if(tem >= 900){
-        $(temp).css({
+      else{
+        $(t).css({
           "width": "60%",
           "height": "70%",
           "top": "15%",
           "left": "20%"
         });
+        $(".dc-lower ul li").delay(3000).css({"border-top": "1px solid #424242"});
         $(".docker-container").css({ "height": "7.5%"});
       }
     });
@@ -164,7 +113,7 @@ $(".hnthvr").hover(function(){
 function abodocfn(adcls){
   $(".udlhnt"+adcls).fadeIn(500).css({"display": "block"});
 }
-$(".hnthvr").mouseleave(function(){
+$(".hnthvr").mouseout(function(){
   $(".udlht").css({"display": "none"});
 });
 
@@ -184,6 +133,36 @@ $(".hnthvr").mouseleave(function(){
   function dotdemopn(dotno){
     $(".dem-lst"+dotno).css("font-size","0.7em");
   }
+
+// Right Side Navigation
+
+var rnbid = $("#rnavbar");
+var bdyid = $(".opend-window");
+$(document).ready(function(){
+  $(".bckimg").click(function(){
+    var tempbdimg = $(this).prop('class').toString();
+    var bcimgno = tempbdimg.substring(tempbdimg.length-2,tempbdimg.length);
+    var bckimgattr = $(".bdimag"+bcimgno).attr('src');
+    $("#mac-bc-img").attr('src',bckimgattr);
+  });
+  $("#opend-window, #mac-bg-image").click(function(){
+    rnbid.animate({ width: "0%"});
+  });
+
+$("#ryt-menu-nav").click(function(){
+  var rnbidper = parseInt(rnbid.css("width"));
+  var bdywth = parseInt(bdyid.css("width"));
+  var t = Math.ceil(rnbidper/bdywth*100);
+  if(t<=0)
+  {
+    rnbid.animate({ width: "22%"});
+  }
+  else {
+    rnbid.animate({ width: "0%"});
+  }
+});
+});
+
 
 
 });
